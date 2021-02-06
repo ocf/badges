558a8a0de5d405b66b1a1b356a3376c6f39fcdef
Nikhil Jha <hi@nikhiljha.com>
2021-02-06 14:25:58 -0800

    :rocket: github events

--- src/badges/main.py
+++ src/badges/main.py
@@ -1,8 +1,11 @@
 from typing import Optional
 from fastapi import FastAPI
-import uvicorn
+from fastapi import Request
+import uvicorn as uv
+import eventkit as ev
 
 app = FastAPI()
+gh_event = ev.Event()
 
 
 @app.get("/")
@@ -10,10 +13,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/webhooks/github")
-def github_webhook():
-    # TODO: Create an event.
+# http post http://localhost:5000/webhooks/github hi=there --json
+@app.post("/webhooks/github")
+async def github_webhook(req: Request):
+    gh_event.emit(await req.json())
     return "OK"
+
+
+def process_github(json):
+    print(json["hi"])
 
 
 @app.get("/items/{item_id}")
@@ -22,4 +30,6 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 
 def main():
-    uvicorn.run("badges.main:app", host="127.0.0.1", port=5000, log_level="info")
+    global gh_event
+    gh_event += process_github
+    uv.run("badges.main:app", host="127.0.0.1", port=5000, log_level="info")
